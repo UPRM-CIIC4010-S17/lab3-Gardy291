1db5373f2539f59ef0ce5df505a5aa3bd2113bd4
4010c6 <4010c6@guayaba.ece.uprm.edu>
2017-02-08 12:42:45 -0400

    imported
    java.awt.Dimension;
    java.awt.Toolkit;
    
    so i could get the dimensions of the monitor and spawn the window in the
    center of the monitor

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,16 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+	public static void main(String[] args)
+	{
+		
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		JFrame myFrame = new JFrame("Edgardo");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation((int)dimension.getWidth()/2, (int)dimension.getHeight()/2);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
